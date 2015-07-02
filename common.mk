ce90813b236d75a2115c7645e69d4c32e7a9428e
Readone !! ErcOne <android-armv7a-belalang-tempur@users.noreply.github.com>
2015-07-02 10:39:03 +0700

    # Include non-open-source parts from google

--- common.mk
+++ common.mk
@@ -204,3 +204,5 @@ $(call inherit-product, hardware/samsung/exynos4210.mk)
 
 # Include non-open-source parts
 $(call inherit-product, vendor/samsung/galaxys2-common/common-vendor.mk)
+# Include non-open-source parts from google
+$(call inherit-product, vendor/google/BoardConfigPartial.mk)
